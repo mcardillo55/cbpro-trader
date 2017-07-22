84e60ca3753ede03b13b70dcdaa7524b2d27400c
Mike Cardillo <mcardillo55@gmail.com>
2017-07-22 05:26:55 -0700

    Moved websocket_queue into TradeAndHeartbeatWebsocket
    Moved the queue into the Websocket subclass to have more precise control over
    it from within the class instance.

--- gdax-trader.py
+++ gdax-trader.py
@@ -13,19 +13,20 @@ import engine
 import config
 import Queue
 
-websocket_queue = Queue.Queue()
 
 
 class TradeAndHeartbeatWebsocket(gdax.WebsocketClient):
     def on_open(self):
         self.products = ["BTC-USD"]
         self.type = "heartbeat"
+        self.websocket_queue = Queue.Queue()
 
     def on_message(self, msg):
         global websocket_queue
 
+    def on_message(self, msg):
         if msg.get('type') == "heartbeat" or msg.get('type') == "match":
-            websocket_queue.put(msg)
+            self.websocket_queue.put(msg)
 
     def on_error(self, e):
         print e
@@ -62,7 +63,7 @@ prev_minute = None
 gdax_websocket.start()
 
 while(True):
-    msg = websocket_queue.get(timeout=1000)
+    msg = gdax_websocket.websocket_queue.get(timeout=1000)
     if msg.get('type') == "match":
         cur_period = process_trade(msg, cur_period)
         indicator_subsys.recalculate_indicators(cur_period)
