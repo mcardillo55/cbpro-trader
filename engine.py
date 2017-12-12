882b881e10c5fe6022bcbc5a772bc212e1ec4c74
Mike Cardillo <mcardillo55@gmail.com>
2017-12-11 16:25:11 -0800

    make trades if 60min MACD > 0

--- engine.py
+++ engine.py
@@ -286,17 +286,19 @@ class TradeEngine():
             new_buy_flag = True
             new_sell_flag = False
             for cur_period in period_list:
-                new_buy_flag = new_buy_flag and Decimal(indicators[cur_period.name]['macd_hist']) > Decimal('0.0')
-                new_sell_flag = new_sell_flag or Decimal(indicators[cur_period.name]['macd_hist']) < Decimal('0.0')
-                if Decimal(indicators[cur_period.name]['adx']) > Decimal(25.0):
-                    # Trending strategy
-                    new_buy_flag = new_buy_flag and Decimal(indicators[cur_period.name]['obv']) > Decimal(indicators[cur_period.name]['obv_ema'])
-                    new_sell_flag = new_sell_flag or Decimal(indicators[cur_period.name]['obv']) < Decimal(indicators[cur_period.name]['obv_ema'])
+                if cur_period.period_size == (60 * 60):
+                    new_buy_flag = new_buy_flag and Decimal(indicators[cur_period.name]['macd_hist']) > Decimal('0.0')
+                    new_sell_flag = new_sell_flag or Decimal(indicators[cur_period.name]['macd_hist']) < Decimal('0.0')
                 else:
-                    # Ranging strategy
-                    new_buy_flag = new_buy_flag and Decimal(indicators[cur_period.name]['stoch_slowk']) > Decimal(indicators[cur_period.name]['stoch_slowd']) and \
-                                   Decimal(indicators[cur_period.name]['stoch_slowk']) < Decimal('50.0')
-                    new_sell_flag = new_sell_flag or Decimal(indicators[cur_period.name]['stoch_slowk']) < Decimal(indicators[cur_period.name]['stoch_slowd'])
+                    if Decimal(indicators[cur_period.name]['adx']) > Decimal(25.0):
+                        # Trending strategy
+                        new_buy_flag = new_buy_flag and Decimal(indicators[cur_period.name]['obv']) > Decimal(indicators[cur_period.name]['obv_ema'])
+                        new_sell_flag = new_sell_flag or Decimal(indicators[cur_period.name]['obv']) < Decimal(indicators[cur_period.name]['obv_ema'])
+                    else:
+                        # Ranging strategy
+                        new_buy_flag = new_buy_flag and Decimal(indicators[cur_period.name]['stoch_slowk']) > Decimal(indicators[cur_period.name]['stoch_slowd']) and \
+                                       Decimal(indicators[cur_period.name]['stoch_slowk']) < Decimal('50.0')
+                        new_sell_flag = new_sell_flag or Decimal(indicators[cur_period.name]['stoch_slowk']) < Decimal(indicators[cur_period.name]['stoch_slowd'])
 
             if product_id == 'LTC-BTC' or product_id == 'ETH-BTC':
                 ltc_or_eth_usd_product = self.get_product_by_product_id(product_id[:3] + '-USD')
