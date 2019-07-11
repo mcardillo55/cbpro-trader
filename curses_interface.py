c6c6982eb56fceaaa4f813a71c18b9749abfd8b9
Mike Cardillo <mike@mjcardillo.com>
2019-07-10 21:21:03 -0700

    Only display BTC and FIAT by default in curses display for simplicity

--- curses_interface.py
+++ curses_interface.py
@@ -24,11 +24,10 @@ class cursesDisplay:
         self.pad.addstr(1, 0, "Waiting for a trade...")
 
     def update_balances(self, trade_engine):
-        self.pad.addstr(0, 0, "%s: %.2f BTC: %.8f ETH: %.8f LTC: %.8f" %
-                        (trade_engine.fiat_currency, trade_engine.fiat, trade_engine.btc,
-                         trade_engine.eth, trade_engine.ltc))
-        self.pad.addstr(1, 0, "BCH: %.8f %s_EQUIV: %.2f" %
-                        (trade_engine.bch, trade_engine.fiat_currency, trade_engine.fiat_equivalent))
+        self.pad.addstr(0, 0, "%s: %.2f BTC: %.8f" %
+                        (trade_engine.fiat_currency, trade_engine.fiat, trade_engine.btc))
+        self.pad.addstr(1, 0, "%s_EQUIV: %.2f" %
+                        (trade_engine.fiat_currency, trade_engine.fiat_equivalent))
 
     def update_candlesticks(self, period_list):
         starty = self.starty
