a0b80e7e68119fc467bcd6d199a336fc6b2cd0d0
Mike Cardillo <mike@mjcardillo.com>
2019-11-24 23:54:00 -0800

    Add redux reducers

---
+++ cbpro-trader/web/src/reducers/chart.js
@@ -0,0 +1,26 @@
+const chart = (state = {period_list: []}, action) => {
+    switch (action.type) {
+        case 'CHANGE_ACTIVE_PERIOD':
+            return {
+                ...state,
+                active_period: action.period_name
+            }
+        case 'ADD_PERIOD':
+            return {
+                ...state,
+                period_list: [...state.period_list, action.period_name]
+            }
+        case 'UPDATE_CANDLESTICKS':
+            return {
+                ...state,
+                candlesticks: action.candlesticks
+            }
+        case 'UPDATE_INDICATORS':
+            return {
+                ...state,
+                indicators: action.indicators
+            }
+        default:
+            return state
+    }
+}
