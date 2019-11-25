f85e86f60d52aedfe4fcedb6102975efef918eef
Mike Cardillo <mike@mjcardillo.com>
2019-11-24 23:53:39 -0800

    Add redux actions

---
+++ cbpro-trader/web/src/actions/index.js
@@ -0,0 +1,19 @@
+export const changeActivePeriod = activePeriod => ({
+    type: 'CHANGE_ACTIVE_PERIOD',
+    period_name
+})
+
+export const addPeriod = period_name => ({
+    type: 'ADD_PERIOD',
+    period_name
+})
+
+export const updateCandlesticks = candlesticks => ({
+    type: 'UPDATE_CANDLESTICKS',
+    candlesticks
+})
+
+export const updateIndicators = period_name => ({
+    type: 'UPDATE_INDICATORS',
+    period_name
+})
