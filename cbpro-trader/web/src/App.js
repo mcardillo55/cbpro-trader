8b1675cf8bceaed4e06806464fd0a72f89b4e1b3
Mike Cardillo <mike@mjcardillo.com>
2019-12-01 11:40:30 -0800

    Rename ChartContainer to be ChartControllerContainer, since it contains the ChartController

--- cbpro-trader/web/src/App.js
+++ cbpro-trader/web/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ChartContainer from './containers/ChartContainer'
+import ChartControllerContainer from './containers/ChartControllerContainer'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ChartContainer />
+      <ChartControllerContainer />
     </div>
   );
 }
