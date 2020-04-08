c186cacf027d2076859f6f09d4dbf6f0d511ba16
Mike Cardillo <mike@mjcardillo.com>
2020-04-07 23:06:05 -0700

    Create Flask app and define routes in web.__init__ instead of manually defining url rules

--- cbpro-trader/interface/web.py
+++ cbpro-trader/interface/web.py
@@ -1,35 +1,31 @@
 from flask import Flask, jsonify
 
-class ProductsView(object):
-    def index(self):
-        return "Hello World!"
-
 class web(object):
     def __init__(self, indicator_subsys, trade_engine):
         self.indicator_subsys = indicator_subsys
         self.trade_engine = trade_engine
-        self.app = Flask(__name__)
-
-        self.app.add_url_rule('/periods/', 'periods', self.periods)
-        self.app.add_url_rule('/periods/<periodName>', 'periods', self.periods)
-        self.app.add_url_rule('/indicators/', 'indicators', self.indicators)
-        self.app.add_url_rule('/indicators/<periodName>', 'indicators', self.indicators)
-        self.app.add_url_rule('/flags/', 'flags', self.flags)
+        app = Flask(__name__)
+        self.app = app
     
-    def periods(self, periodName=None):
-        return jsonify(self.indicator_subsys.get_period_data(periodName))
+        @app.route('/periods/')
+        @app.route('/periods/<periodName>')
+        def periods(periodName=None):
+            return jsonify(self.indicator_subsys.get_period_data(periodName))
 
-    def indicators(self, periodName=None):
-        return jsonify(self.indicator_subsys.get_indicator_data(periodName))
+        @app.route('/indicators/')
+        @app.route('/indicators/<periodName>')
+        def indicators(periodName=None):
+            return jsonify(self.indicator_subsys.get_indicator_data(periodName))
 
-    def flags(self):
-        flags = {}
-        for product in self.trade_engine.products:
-            if product.buy_flag is True:
-                flags[product.product_id] = "buy"
-            else:
-                flags[product.product_id] = "sell"
-        return jsonify(flags)
+        @app.route('/flags/')
+        def flags():
+            flags = {}
+            for product in self.trade_engine.products:
+                if product.buy_flag is True:
+                    flags[product.product_id] = "buy"
+                else:
+                    flags[product.product_id] = "sell"
+            return jsonify(flags)
 
     def start(self):
         self.app.run(host='0.0.0.0')
