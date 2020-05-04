2d57beddbe8706aacd88a0aab9a05fd9b6a531c5
Mike Cardillo <mike@mjcardillo.com>
2020-05-04 14:17:50 -0700

    web: rearranging data in orders tables

--- cbpro-trader/web/src/components/Orders.jsx
+++ cbpro-trader/web/src/components/Orders.jsx
@@ -7,21 +7,23 @@ function Orders(props) {
             <h2 className="first-h2">Orders</h2>
             <table>
                 <tr>
+                    <th>Product</th>
                     <th>Side</th>
                     <th>Size</th>
-                    <th>Filled</th>
-                    <th>Fees</th>
                     <th>Price</th>
+                    <th>Filled</th>
+                    <th>Fee</th>
                 </tr>
             {
                 orders['orders'].map((order) => {
                     return(
-                        <tr>    
-                            <td>{order.side}</td>
+                        <tr>
+                            <td>{order.product_id}</td>
+                            <td>{order.side.toUpperCase()}</td>
                             <td>{order.size}</td>
+                            <td>{parseFloat(order.price).toFixed(2)}</td>
                             <td>{order.filled_size}</td>
                             <td>{parseFloat(order.fill_fees).toFixed(8)}</td>
-                            <td>{parseFloat(order.price).toFixed(2)}</td>
                         </tr>
                     )
                 })
@@ -33,18 +35,19 @@ function Orders(props) {
                     <th>Product</th>
                     <th>Side</th>
                     <th>Size</th>
-                    <th>Fee</th>
                     <th>Price</th>
+                    <th>Fee</th>
+
                 </tr>  
             {
                 orders['fills'].map((fill) => {
                     return(
                         <tr>
                             <td>{fill.product_id}</td>
-                            <td>{fill.side}</td>
+                            <td>{fill.side.toUpperCase()}</td>
                             <td>{fill.size}</td>
-                            <td>{parseFloat(fill.fee).toFixed(8)}</td>
                             <td>{parseFloat(fill.price).toFixed(2)}</td>
+                            <td>{parseFloat(fill.fee).toFixed(8)}</td>
                         </tr>
                     )
                 })
